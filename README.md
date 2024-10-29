d0eb999bb43d339a45e6396e4991903aa6311141
mirsazzathossain@gmail.com <kishor25@g.skku.skku.edu>
2024-10-29 23:07:54 +0600

    Update

---
+++ README.md
@@ -0,0 +1,3 @@
+```bash
+python main.py --dataset gowalla-all --read_pkl False --grid_size 40 --d_model 128 --n_heads 5 --n_layers 2
+```
--- models/molnet.py
+++ models/molnet.py
@@ -1,8 +1,14 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from layers import GraphConvolution
-from layers import PositionalEncoding, get_attn_pad_mask, EncoderLayer, complexSparsemax
+
+from .layers import (
+    EncoderLayer,
+    GraphConvolution,
+    PositionalEncoding,
+    complexSparsemax,
+    get_attn_pad_mask,
+)
 
 
 class GcnNet(nn.Module):
@@ -53,7 +59,8 @@ class EncoderAttentionBlock(nn.Module):
         super(EncoderAttentionBlock, self).__init__()
         self.pos_emb = PositionalEncoding(d_model)
         self.layers = nn.ModuleList(
-            [EncoderLayer(d_model, d_k, d_v, d_ff, n_heads) for _ in range(n_layers)])
+            [EncoderLayer(d_model, d_k, d_v, d_ff, n_heads) for _ in range(n_layers)]
+        )
 
     def forward(self, input_seq, input_seq_emb):
         """[forward function]
@@ -65,8 +72,7 @@ class EncoderAttentionBlock(nn.Module):
         Returns:
             [type]: [description]
         """
-        enc_outputs = self.pos_emb(
-            input_seq_emb.transpose(0, 1)).transpose(0, 1)
+        enc_outputs = self.pos_emb(input_seq_emb.transpose(0, 1)).transpose(0, 1)
         enc_self_attn_mask = get_attn_pad_mask(input_seq, input_seq)
         for layer in self.layers:
             # output: [batch_size, max_len, d_model]
@@ -105,17 +111,19 @@ class ElasticAttentionBlock(nn.Module):
         Returns:
             [torch.tensor]: [context]
         """
-        if self.attn_strategy == 'cos':
-            score = torch.matmul(querry_emb, traj_emb.T) / torch.matmul(torch.sqrt(torch.sum(querry_emb *
-                                                                                             querry_emb, dim=1)).unsqueeze(1), torch.sqrt(torch.sum(traj_emb * traj_emb, dim=1)).unsqueeze(0))
-        elif self.attn_strategy == 'dot':
+        if self.attn_strategy == "cos":
+            score = torch.matmul(querry_emb, traj_emb.T) / torch.matmul(
+                torch.sqrt(torch.sum(querry_emb * querry_emb, dim=1)).unsqueeze(1),
+                torch.sqrt(torch.sum(traj_emb * traj_emb, dim=1)).unsqueeze(0),
+            )
+        elif self.attn_strategy == "dot":
             score = torch.matmul(querry_emb, traj_emb.T)
 
-        if self.softmax_strategy == 'complex':
+        if self.softmax_strategy == "complex":
             atten = self.complexSoftmax(score)
-        elif self.softmax_strategy == 'simple':
+        elif self.softmax_strategy == "simple":
             pass  # todo
-        elif self.softmax_strategy == 'norm':
+        elif self.softmax_strategy == "norm":
             atten = self.normSoftmax(score)
 
         context = torch.matmul(atten, traj_emb)
@@ -152,8 +160,14 @@ class Embedding(nn.Module):
         Returns:
             [torch.tensor]: [fusion embedding]
         """
-        embedding = self.tanh(self.fc(torch.cat(
-            [input_seq_emb, self.timeEmb(time_seq), self.stateEmb(state_seq)], dim=-1)))
+        embedding = self.tanh(
+            self.fc(
+                torch.cat(
+                    [input_seq_emb, self.timeEmb(time_seq), self.stateEmb(state_seq)],
+                    dim=-1,
+                )
+            )
+        )
         return embedding
 
 
@@ -164,7 +178,19 @@ class MolNet(nn.Module):
         nn ([type]): [description]
     """
 
-    def __init__(self, Attn_Strategy, Softmax_Strategy, Pool_Strategy, d_model, d_k, d_v, d_ff, n_heads, n_layers, user_nums):
+    def __init__(
+        self,
+        Attn_Strategy,
+        Softmax_Strategy,
+        Pool_Strategy,
+        d_model,
+        d_k,
+        d_v,
+        d_ff,
+        n_heads,
+        n_layers,
+        user_nums,
+    ):
         """[summary]
 
         Args:
@@ -184,13 +210,11 @@ class MolNet(nn.Module):
 
         self.embedding = Embedding(d_model)
         self.LocalEncoderNet = EncoderAttentionBlock(
-            d_model, d_k, d_v, d_ff, n_heads, n_layers)
-        self.GlobalAttentionNet = ElasticAttentionBlock(
-            Attn_Strategy, Softmax_Strategy)
+            d_model, d_k, d_v, d_ff, n_heads, n_layers
+        )
+        self.GlobalAttentionNet = ElasticAttentionBlock(Attn_Strategy, Softmax_Strategy)
         self.classifier = nn.Sequential(
-            nn.Linear(d_model*2, d_model),
-            nn.ReLU(),
-            nn.Linear(d_model, user_nums)
+            nn.Linear(d_model * 2, d_model), nn.ReLU(), nn.Linear(d_model, user_nums)
         )
 
     def forward(self, grid_emb, traj_emb, input_seq, time_seq, state_seq, input_index):
@@ -208,7 +232,8 @@ class MolNet(nn.Module):
             [torch.tensor]: [the result of trajectories]
         """
         input_seq_onehot = torch.zeros(
-            input_seq.shape[0], input_seq.shape[1], grid_emb.shape[0]).cuda()
+            input_seq.shape[0], input_seq.shape[1], grid_emb.shape[0]
+        ).cuda()
         for idx, one_input in enumerate(input_seq):
             for idy, index in enumerate(one_input[one_input != -1]):
                 input_seq_onehot[idx, idy, index] = 1
@@ -218,7 +243,7 @@ class MolNet(nn.Module):
 
         input_seq_emb = self.LocalEncoderNet(input_seq, input_seq_emb)
 
-        if self.pool_strategy == 'max':
+        if self.pool_strategy == "max":
             input_seq_emb, _ = input_seq_emb.max(dim=1)
         else:
             input_seq_emb = input_seq_emb.mean(dim=1)
